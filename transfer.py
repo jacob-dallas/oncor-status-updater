e35b389840d3037a4d0e313d5a3a1f799ef1a0f4
Pavelka <jacob.pavelka@dallascityhall.com>
2023-04-28 07:28:05 -0500

    added continuous scan functionality

--- transfer.py
+++ transfer.py
@@ -11,79 +11,80 @@ import time
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, ElementClickInterceptedException, TimeoutException
 from account_disconnect import check_active, check_restored
 
-startnumber = 0
+def full_scan():
+    startnumber = 0
 
-start_time = datetime.datetime.now()
-load_time = 1
-service = Service(executable_path="chromedriver.exe")
-options = Options()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-options.accept_insecure_certs=True
-driver = webdriver.Chrome(service=service, options=options)
-driver.implicitly_wait(1)
-outages = pd.read_excel('Traffic Signal Spreadsheets.xlsx','ONCOR', converters={"ESI ID": str})
-outage_log = open('outage_log.txt','w')
-last_complete_entry = 0
-try:
-    for i,id in enumerate(outages['ESI ID']):
-        if i < startnumber:
-            continue
-        percent_complete = i/outages['ESI ID'].size*100
-        print(f'{percent_complete:.2f}%')
-        outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\": beginning search\n')
-        try:
-            driver.get("https://www.oncor.com/outages/check_status/identify/esi")
-            id_input = driver.find_element(value='esi_id_text')
-            next_button = driver.find_element(value='next')
-            id_input.clear()
-            id_input.send_keys(id)
-            next_button.click()
-            #add check to see if account is "unregistered"
-            time.sleep(load_time)
-            next_button = WebDriverWait(
-                driver, 
-                5
-            ).until(
-                expected_conditions.presence_of_element_located((By.ID, 'next'))
-            )
-            next_button.click()
+    start_time = datetime.datetime.now()
+    load_time = 1
+    service = Service(executable_path="chromedriver.exe")
+    options = Options()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.accept_insecure_certs=True
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.implicitly_wait(1)
+    outages = pd.read_excel('Traffic Signal Spreadsheets.xlsx','ONCOR', converters={"ESI ID": str})
+    outage_log = open('outage_log.txt','w')
+    last_complete_entry = 0
+    try:
+        for i,id in enumerate(outages['ESI ID']):
+            if i < startnumber:
+                continue
+            percent_complete = i/outages['ESI ID'].size*100
+            print(f'{percent_complete:.2f}%')
+            outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\": beginning search\n')
             try:
+                driver.get("https://www.oncor.com/outages/check_status/identify/esi")
+                id_input = driver.find_element(value='esi_id_text')
+                next_button = driver.find_element(value='next')
+                id_input.clear()
+                id_input.send_keys(id)
+                next_button.click()
+                #add check to see if account is "unregistered"
                 time.sleep(load_time)
-                status = WebDriverWait(
+                next_button = WebDriverWait(
                     driver, 
                     5
                 ).until(
-                    expected_conditions.presence_of_element_located((By.ID, 'label_no_omscall_fault'))
-                )  
-        
-                status = status.text
-                #program restored power
-            except (NoSuchElementException, TimeoutException) as error:
-                status = check_restored(driver)
+                    expected_conditions.presence_of_element_located((By.ID, 'next'))
+                )
+                next_button.click()
+                try:
+                    time.sleep(load_time)
+                    status = WebDriverWait(
+                        driver, 
+                        5
+                    ).until(
+                        expected_conditions.presence_of_element_located((By.ID, 'label_no_omscall_fault'))
+                    )  
+            
+                    status = status.text
+                    #program restored power
+                except (NoSuchElementException, TimeoutException) as error:
+                    status = check_restored(driver)
 
-            outages['Status'][i] = status
-            outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\" status: {status}\n')
-        except StaleElementReferenceException as error:
-            outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\": [ERROR] search was too fast, status was not updated\n')
-        except ElementClickInterceptedException as error:
-            msg = check_active(driver)
-            if msg == 'acount_active':
-                raise Exception
-            outage_log.write(f'{datetime.datetime.now()}: [ERROR] {msg}\n')
-        last_complete_entry = i
-except Exception as error:
-    print("an error occured\n")
-    print(error)
-    outage_log.write(f'{datetime.datetime.now()}: [ERROR] a fatal error occured\n')
-finally:
+                outages['Status'][i] = status
+                outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\" status: {status}\n')
+            except StaleElementReferenceException as error:
+                outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\": [ERROR] search was too fast, status was not updated\n')
+            except ElementClickInterceptedException as error:
+                msg = check_active(driver)
+                if msg == 'acount_active':
+                    raise Exception
+                outage_log.write(f'{datetime.datetime.now()}: [ERROR] {msg}\n')
+            last_complete_entry = i
+    except Exception as error:
+        print("an error occured\n")
+        print(error)
+        outage_log.write(f'{datetime.datetime.now()}: [ERROR] a fatal error occured\n')
+    finally:
 
-    finish_time = datetime.datetime.now()
-    delta = finish_time-start_time
-    outage_log.write(f'updating {last_complete_entry} meters took {delta.seconds} seconds')
-    outages.to_excel('excel_out.xlsx','sheet1',index=False)
-    outage_log.close()
-    driver.quit()
+        finish_time = datetime.datetime.now()
+        delta = finish_time-start_time
+        outage_log.write(f'updating {last_complete_entry} meters took {delta.seconds} seconds')
+        outages.to_excel('excel_out.xlsx','sheet1',index=False)
+        outage_log.close()
+        driver.quit()
 
 
-print(delta)
+    print(delta)
