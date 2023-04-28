4a182acf6f75ec8093b0a941f9faed76d5794ad9
Pavelka <jacob.pavelka@dallascityhall.com>
2023-04-28 07:23:17 -0500

    fixed duplicate id error

--- transfer.py
+++ transfer.py
@@ -8,7 +8,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import time 
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, ElementClickInterceptedException, TimeoutException
+from account_disconnect import check_active, check_restored
+
+startnumber = 0
 
 start_time = datetime.datetime.now()
 load_time = 1
@@ -24,22 +27,23 @@ outage_log = open('outage_log.txt','w')
 last_complete_entry = 0
 try:
     for i,id in enumerate(outages['ESI ID']):
-        percent_complete = i/outages['ESI ID'].size
+        if i < startnumber:
+            continue
+        percent_complete = i/outages['ESI ID'].size*100
         print(f'{percent_complete:.2f}%')
         outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\": beginning search\n')
         try:
             driver.get("https://www.oncor.com/outages/check_status/identify/esi")
             id_input = driver.find_element(value='esi_id_text')
             next_button = driver.find_element(value='next')
+            id_input.clear()
             id_input.send_keys(id)
             next_button.click()
             #add check to see if account is "unregistered"
-            ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
             time.sleep(load_time)
             next_button = WebDriverWait(
                 driver, 
-                5,
-                ignored_exceptions=ignored_exceptions
+                5
             ).until(
                 expected_conditions.presence_of_element_located((By.ID, 'next'))
             )
@@ -48,24 +52,30 @@ try:
                 time.sleep(load_time)
                 status = WebDriverWait(
                     driver, 
-                    5,
-                    ignored_exceptions=ignored_exceptions
+                    5
                 ).until(
                     expected_conditions.presence_of_element_located((By.ID, 'label_no_omscall_fault'))
                 )  
         
                 status = status.text
-            except NoSuchElementException as error:
-                status = 'power failure'
+                #program restored power
+            except (NoSuchElementException, TimeoutException) as error:
+                status = check_restored(driver)
 
             outages['Status'][i] = status
             outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\" status: {status}\n')
         except StaleElementReferenceException as error:
             outage_log.write(f'{datetime.datetime.now()}: ESI ID \"{id}\": [ERROR] search was too fast, status was not updated\n')
+        except ElementClickInterceptedException as error:
+            msg = check_active(driver)
+            if msg == 'acount_active':
+                raise Exception
+            outage_log.write(f'{datetime.datetime.now()}: [ERROR] {msg}\n')
         last_complete_entry = i
-except:
-    print("an error occured")
-    outage_log.write(f'{datetime.datetime.now()}: a fatal error occured')
+except Exception as error:
+    print("an error occured\n")
+    print(error)
+    outage_log.write(f'{datetime.datetime.now()}: [ERROR] a fatal error occured\n')
 finally:
 
     finish_time = datetime.datetime.now()
