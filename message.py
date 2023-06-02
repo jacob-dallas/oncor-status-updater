c3265c5e583f7fed31638768658e34df5579a180
Pavelka <jacob.pavelka@dallascityhall.com>
2023-06-02 14:25:28 -0500

    added status updates to exe

--- message.py
+++ message.py
@@ -12,10 +12,7 @@ class MessageAnnouncer:
 
     def announce(self, msg):
         for i in reversed(range(len(self.listeners))):
-            try:
-                self.listeners[i].put_nowait(msg)
-            except queue.Full:
-                del self.listeners[i]
+            self.listeners[i].put(msg)
 
 def format_sse(data: str, event=None) -> str:
     msg = f'data: {data}\n\n'
